111bc88afd4bcd197dd0edb0ac6fb1f49414dcc3
Petre Stefan <p022386k@student.staffs.ac.uk>
2020-10-04 01:13:09 +0300

    Weekend

---
+++ Portofolio/Projekt3_DrawingShapes/Projekt3_DrawingShapes.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x1,x2,y1,y2;
+	cin >> x1;
+	cin >> y1;
+	cin >> x2;
+	cin >> y2;
+	int temp;
+	if (x1 > x2) 
+	{
+		temp = x1;
+		x1 = x2;
+		x2 = temp;	
+	}
+	if (y1 > y2)
+	{
+		temp = y1;
+		y1 = y2;
+		y2 = temp;
+	}
+	for (int i = 0; i < 21; i++)
+	{
+		cout << "@@";
+	}
+	cout << endl;
+	for (int i = 0; i < 20; i++) 
+	{
+		cout << "@";
+		for (int j = 0; j < 20; j++)
+		{
+			if (i == x1 && j >= y1 && j <= y2 || i == x2 && j >= y1 && j <= y2
+				|| j == y1 && i >= x1 && i <= x2 || j == y2 && i >= x1 && i <= x2)
+			{
+				cout << "#";
+			}
+			else {
+				cout << " ";
+			}
+			cout << " ";
+		}
+		cout << "@";
+		cout << endl;
+	}
+	for (int i = 0; i < 21; i++)
+	{
+		cout << "@@";
+	}
+	cout << endl;
+}
