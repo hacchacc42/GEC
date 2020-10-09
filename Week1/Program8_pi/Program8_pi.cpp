bf936fec2ae3748c780d27401d29ec553f71d7ed
Petre Stefan <p022386k@student.staffs.ac.uk>
2020-10-09 14:23:08 +0300

    week2.1

---
+++ Week1/Program8_pi/Program8_pi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const float pi = 3.14159f;
+    float area, radius;
+    radius = 25.0f;
+    area = pi * radius * radius;
+
+    cout << "The area of a circle with a radius of " << radius << " is " << area << endl;
+
+    return 0;
+}
